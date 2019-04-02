8b957790bc624485949973c8bcd72c7802047bf9
Alfred <alfred@DMSC224.local>
2019-04-02 11:20:52 -0700

    add function list

---
+++ R/n1pas.R
@@ -0,0 +1,17 @@
+#' \code{n1pas}: An R Package for N-of-1-\emph{pathways} Alternatively Spliced
+#'
+#' The n1pas package provides functions to run N1PAS:
+#' 
+#' @section N-of-1-\emph{pathways} functions:
+#' \itemize{
+#' \code{\link{compute_multi_nof1_pathways}}
+#' \code{\link{compute_nof1_md}}
+#' \code{\link{compute_nof1_pathways}}
+#' \code{\link{compute_nof1_wilcoxon}}
+#' \code{\link{compute_vert_md}}
+#' \code{\link{compute_zscore}}
+#' }
+#' 
+#' @docType package
+#' @name n1pas
+NULL
