fe1c9d438830d6750d80e9a4706db0be2a4989c0
Alfred <alfred@DMSC22440N9HTDF.rd.unr.edu>
2019-04-02 16:23:13 -0700

    run devtools::document() after revising docs

--- R/n1pas.R
+++ R/n1pas.R
@@ -2,14 +2,12 @@
 #'
 #' The n1pas package provides functions to run N1PAS:
 #' 
-#' @section N1PAS functions:
-#' \itemize{
+#' N1PAS functions:
 #' \code{\link{compute_hellinger}}
-#' \code{\link{get_OR}
+#' \code{\link{get_OR}}
 #' \code{\link{transform_iso_gene}}
 #' \code{\link{transform_gene_pathway}}
 #' \code{\link{compute_n1pas}}
-#' }
 #' 
 #' @docType package
 #' @name n1pas
